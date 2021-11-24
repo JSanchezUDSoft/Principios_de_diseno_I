bf3395f32e136111e6d01604ed6883fcc1c343ba
DogMey <68884758+DogMey@users.noreply.github.com>
2021-11-23 19:57:18 -0500

    Actualizar Prestamo completo con posible error :v

--- Principios_de_diseno/src/Principal/Prestamo.java
+++ Principios_de_diseno/src/Principal/Prestamo.java
@@ -1,23 +1,25 @@
 package Principal;
 
 public class Prestamo{
-    private String Id_Prestamo;
-    private double Valor;
-    private String Fecha_Pago;
-    private String Fecha_Aut;
-    private String Fecha_Entrega;
-    private double Dinero_Banco;
+    private String  Id_Prestamo;
+    private double  Valor;
+    private String  Fecha_Pago;
+    private String  Fecha_Aut;
+    private String  Fecha_Entrega;
+    private double  Dinero_Banco;
+    private int     defDinero = 0;
 
-    public Prestamo() {
-    }
+    Scanner lec = new Scanner(System.in);//Puede tener error :v
+
+    public Prestamo() {}
 
     public Prestamo(String id_Prestamo, double valor, String fecha_Pago, String fecha_Aut, String fecha_Entrega, double dinero_Banco) {
-        Id_Prestamo = id_Prestamo;
-        Valor = valor;
-        Fecha_Pago = fecha_Pago;
-        Fecha_Aut = fecha_Aut;
-        Fecha_Entrega = fecha_Entrega;
-        Dinero_Banco = dinero_Banco;
+        this.Id_Prestamo = id_Prestamo;
+        this.Valor = valor;
+        this.Fecha_Pago = fecha_Pago;
+        this.Fecha_Aut = fecha_Aut;
+        this.Fecha_Entrega = fecha_Entrega;
+        this.Dinero_Banco = dinero_Banco;
     }
 
     public String getId_Prestamo() {
@@ -25,7 +27,18 @@ public class Prestamo{
     }
 
     public void setId_Prestamo(String id_Prestamo) {
-        Id_Prestamo = id_Prestamo;
+        try{
+            while(id_Prestamo <= 0){
+                System.out.println("ID INCORRECTO: ");
+                System.out.println("Debe ingresar un ID mayor a 0, intente nuevamente: ");
+                id_Prestamo = lec.nextInt();
+            }
+
+            this.Id_Prestamo = id_Prestamo;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public double getValor() {
@@ -33,7 +46,40 @@ public class Prestamo{
     }
 
     public void setValor(double valor) {
-        Valor = valor;
+        try{
+            double disponible;
+            int op;
+            while(valor<=0){
+                System.out.println("VALOR DEL PRESTAMO INCORRECTO: ");
+                System.out.println("Debe ingresar un valor mayor a 0, intente nuevamente: ");
+                valor = lec.nextDouble();
+            }
+            disponible = getDinero_Banco() - valor;
+            if(disponible < 0){
+                System.out.println("El banco le puede prestar: " + getDineroBanco());
+                System.out.println("Digite 1 si acepta ese valor, otro número si prefiere volver despues: ");
+                op = lec.nextInt();
+                if(op == 1){
+                    this.Valor = getDinero_Banco();
+                    setDinero_Banco(0);
+                    setDefDinero(2);
+                }
+                else{
+                    this.Valor = 0;
+                }
+            }
+            else{
+                this.Valor = valor;
+                setDinero_Banco(getDinero_Banco() - valor);
+                if(getDinero_Banco() == 0){
+                    System.out.println("El banco se quedo sin recursos");
+                    setDefDinero(2);
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public String getFecha_Pago() {
@@ -66,5 +112,13 @@ public class Prestamo{
 
     public void setDinero_Banco(double dinero_Banco) {
         Dinero_Banco = dinero_Banco;
+    }
+
+    public int getDefDinero() {
+        return defDinero;
+    }
+
+    public void setDefDinero(int defDinero) {
+        this.defDinero = defDinero;
     }
 }
