a14f4929f0590718ea93557520aa28233521876a
Diego-1116 <72049443+Diego-1116@users.noreply.github.com>
2021-11-23 20:41:33 -0500

    Main terminado

--- Principios_de_diseno/src/Principal/Main.java
+++ Principios_de_diseno/src/Principal/Main.java
@@ -3,6 +3,27 @@ package Principal;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try{
+            G_Prestamo p = new G_Prestamo();
+            boolean menu = true;
+
+            while(menu == true){
+                if(p.getDefDinero() == 0 || p.getDefDinero() == 1){
+                    p.capturar();
+                    p.imprimir();
+                }
+                else if(p.getDefDinero() == 2){
+                    System.out.prinln("\nSe agotaron los recursos del banco, intente mañana");
+                    menu = false;
+                }
+                else{
+                    System.out.println("\nEjecucion finalizada correctamente");
+                    menu = false;
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
